462a782078163b015cb249b3dfbc95b0458a2e3c
Sandrex233 <sandrex233@gmail.com>
2023-01-14 18:22:32 +0400

    review slider

--- script.js
+++ script.js
@@ -1,3 +1,111 @@
+/* Slider */
+
+const slider = document.querySelector('.reviews__card-mobile'),
+    slides = Array.from(document.querySelectorAll('.reviews__card-item')).slice(4)
+const btns = Array.from(document.querySelectorAll('.reviews__card-pgn-btn'))
+
+let isDragging = false,
+    startPos = 0,
+    currentTranslate = 0,
+    prevTranslate = 0,
+    animationID = 0,
+    currentIndex = 0
+
+for (let i = 0; i < slides.length; i++) {
+    slides[i].style.transform = `translateX(${i * 20 + "px"})`
+}
+
+btns[0].classList.add('btnactive')
+
+
+slides.forEach((slide, index) => {
+    const slideImage = slide.querySelector('img')
+    slideImage.addEventListener('dragstart', (e) => e.preventDefault())
+
+    // Touch events
+    slide.addEventListener('touchstart', touchStart(index))
+    slide.addEventListener('touchend', touchEnd)
+    slide.addEventListener('touchmove', touchMove)
+
+    // Mouse events
+    slide.addEventListener('mousedown', touchStart(index))
+    slide.addEventListener('mouseup', touchEnd)
+    slide.addEventListener('mouseleave', touchEnd)
+    slide.addEventListener('mousemove', touchMove)
+})
+
+// Disable context menu
+window.oncontextmenu = function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    return false
+}
+
+function touchStart(index) {
+    return function (event) {
+        currentIndex = index
+        startPos = getPositionX(event)
+        isDragging = true
+
+        animationID = requestAnimationFrame(animation)
+        slider.classList.add('grabbing')
+    }
+}
+
+function touchEnd() {
+    isDragging = false
+    cancelAnimationFrame(animationID)
+
+    const movedBy = currentTranslate - prevTranslate
+
+    if (movedBy < -100 && currentIndex < slides.length - 1) currentIndex += 1
+
+    if (movedBy > 100 && currentIndex > 0) currentIndex -= 1
+
+    setPositionByIndex()
+
+    btns[currentIndex].classList.add('btnactive')
+
+    if (currentIndex != 0) {
+        btns[currentIndex - 1].classList.remove('btnactive')
+    }
+    if (currentIndex != btns.length - 1) {
+        btns[currentIndex + 1].classList.remove('btnactive')
+    }
+
+    slider.classList.remove('grabbing')
+}
+
+function touchMove(event) {
+    if (isDragging) {
+        const currentPosition = getPositionX(event)
+        currentTranslate = prevTranslate + currentPosition - startPos
+    }
+}
+
+function getPositionX(event) {
+    return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
+}
+
+function animation() {
+    setSliderPosition()
+    if (isDragging) requestAnimationFrame(animation)
+}
+
+function setSliderPosition() {
+    slider.style.transform = `translateX(${currentTranslate}px)`
+}
+
+function setPositionByIndex() {
+    currentTranslate = currentIndex * - 300
+    prevTranslate = currentTranslate
+    setSliderPosition()
+}
+
+
+
+/* FAQ */
+
 var acc = document.getElementsByClassName("faq__accordion-item");
 var i;
 
